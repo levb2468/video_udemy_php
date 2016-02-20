261cf1ea39124d49da48f96ac450e8efcba76589
Luis Vicente <levb2468@gmail.com>
2016-02-20 10:35:37 -0500

    segundo commit: elseif_form

---
+++ elseif_form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>elseif form</title>
+	<link rel="stylesheet" href="estilos.css">
+
+</head>
+<body>
+	<div class="contenedor">
+		<form action="ejercicio1.php" method="POST">
+			<p>
+				<label for="">Promedio</label>
+				<input type="text" name="Promedio">
+			</p>
+			<p>
+				<button>Calcular</button>	
+			</p>
+
+		</form>
+		
+	</div>
+</body>
+</html>
